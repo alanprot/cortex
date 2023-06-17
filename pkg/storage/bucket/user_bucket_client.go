df7dad655fa7062d28dea00b6a59ca187597ef13
Alan Protasio <alanprot@gmail.com>
2023-06-16 17:29:38 -0700

    add test

--- pkg/storage/bucket/user_bucket_client.go
+++ pkg/storage/bucket/user_bucket_client.go
@@ -4,14 +4,14 @@ import (
 	"github.com/thanos-io/objstore"
 )
 
-type BucketClient interface {
+type UserBucketClient interface {
 	objstore.InstrumentedBucket
 	SSEBucket
 }
 
 // NewUserBucketClient returns a bucket client to use to access the storage on behalf of the provided user.
 // The cfgProvider can be nil.
-func NewUserBucketClient(userID string, bucket objstore.Bucket, cfgProvider TenantConfigProvider) BucketClient {
+func NewUserBucketClient(userID string, bucket objstore.Bucket, cfgProvider TenantConfigProvider) UserBucketClient {
 	// Inject the user/tenant prefix.
 	bucket = NewWrappedBucket(bucket, NewPrefixedBucketClient(bucket, userID))
 
